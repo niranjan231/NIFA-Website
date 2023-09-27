9bb282f37b722f9939761c081223de8b7eb3cef0
hp <niranjan962796@gmail.com>
2023-09-27 12:49:39 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,154 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <div>
+      <div className="first">
+        <h6 className="h-5">Welcome to the National Institute of Fine Arts</h6>
+        <p>
+          {" "}
+          <img
+            className="img-1"
+            src="https://pngimg.com/uploads/phone/phone_PNG48994.png"
+          ></img>{" "}
+          9555112200
+        </p>
+        <p>
+          {" "}
+          <img
+            className="img-2"
+            src="https://pngimg.com/uploads/phone/phone_PNG48994.png"
+          ></img>{" "}
+          9555112200
+        </p>
+        <p>
+          {" "}
+          <img
+            className="img-3"
+            src="https://pngimg.com/uploads/phone/phone_PNG48994.png"
+          ></img>{" "}
+          admission@nifafinearts.com
+        </p>
+        <img
+          className="img-4"
+          src="https://tse4.mm.bing.net/th?id=OIP.Hj8atp08zsZaOWABBv2JzAHaHa&pid=Api&P=0&h=180"
+        ></img>
+        <img
+          className="img-5"
+          src="https://www.pngkey.com/png/full/2-27646_twitter-logo-png-transparent-background-logo-twitter-png.png"
+        ></img>
+        <img
+          className="img-6"
+          src="https://tse2.mm.bing.net/th?id=OIP.XgT9d552B6R9C60Ul2yqkAHaHa&pid=Api&P=0&h=180"
+        ></img>
+      </div>
+      <div className="second">
+        <img
+          className="img-7"
+          src="https://www.nifafinearts.com/images/nifa-new-logo.png"
+        ></img>
+        <a className="link-1" href="https://www.nifafinearts.com/">
+          Home
+        </a>
+        <a className="link-1" href="https://www.nifafinearts.com/about-us.php">
+          About
+        </a>
+        <a className="link-1" href="https://www.nifafinearts.com/course.php">
+          Course
+        </a>
+        {/* <option>B.A</option>
+  <option>B.Sc</option>
+  <option>B.C.A</option>
+  <option>B.Ed</option>
+  <option>B.Com</option>
+  <option>L.L.B</option>
+  <option>B.Tech</option>
+  <option>M.Tech</option>
+  </select>Course</a> */}
+        <a className="link-1" href="https://www.nifafinearts.com/about-us.php">
+          Activity
+        </a>
+        <a className="link-1" href="https://www.nifafinearts.com/about-us.php">
+          Admisson
+        </a>
+        <a className="link-1" href="https://www.nifafinearts.com/about-us.php">
+          Online Classes
+        </a>
+        <a className="link-1" href="https://www.nifafinearts.com/about-us.php">
+          Video
+        </a>
+      </div>
+      <div className="third">
+        <img
+          className="img-8"
+          src="https://wallpaperaccess.com/full/499246.jpg"
+        ></img>
+      </div>
+      <p className="mar-1">
+        <marquee>
+          {" "}
+          Please download the NIFA app from our online classes page to register
+          for a complimentary trial class
+        </marquee>
+      </p>
+      <h2>Welcome To National Institute of Fine Arts</h2>
+      <p className="p-1">
+        National Institute of Fine Arts (NIFA) is a national center for
+        education in arts, fostering the excellence of emerging and established
+        artists and advancing art to create a more human world. National
+        Institute of Fine Arts (NIFA) was established by MRS RENU KHERA in Delhi
+        and NCR of Delhi on July 2005. Institute established another unit in
+        Panna (M.P) which is affiliated to Raja Mansingh Music and Arts
+        University (Gwalior). Its aim is to provide education and training to
+        students on a wide spectrum of Fine Arts. National Institute of Fine
+        Arts is a place where the creative expression of individuals is nurtured
+        and a sense of community flourishes. We seek to shape the global future
+        of arts with an emphasis on excellence that allows its members to reach
+        for the highest artistic standards as individuals while recognizing that
+        the Art is one of the foundations of a healthy and creative society.
+        This is a place where national and international leaders in the arts
+        gather, teach, show and perform their work. The institute prides itself
+        on its openness and on creating an environment that is safe, welcoming,
+        and built on mutual respect.
+      </p>
+      <div className="online">
+<h1>Online Classes</h1>
+<p className="p-2">Dear Students/Parents, Nifa presents its Learning App for your regular art <br></br>practice in which we have added many amazing features Like: Step-by-step <br></br>recorded demo videos prepared by your faculty members. Regular assessment of your<br></br> practise through assignment section. Live class section from same app. <br></br>Attendance, payment invoice, art news, announcements, important information and direct <br></br>connect with the management team are some more features. So hurry up n <br></br>join now to experience personalised coaching by experts sitting at your home<br></br>
+
+We wish u Happy Learning. Stay Safe n Healthy.</p>
+<img className="img-9" src="https://www.nifafinearts.com/images/gplay.png"></img>
+<img className="img-10" src="https://www.nifafinearts.com/images/appstore.png"></img>
+      </div>
+      <div className="inner">
+      <div class="card">
+  <img src="https://www.nifafinearts.com/images/animation-sketching.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <p class="card-text">6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
+    <button className="btn-1">Read More</button>
+  </div>
+  </div>
+  <div class="card">
+  <img src="https://www.nifafinearts.com/images/3mchc.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <p class="card-text">6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
+    <button className="btn-1">Read More</button>
+  </div>
+  </div>
+  <div class="card">
+  <img src="https://www.nifafinearts.com/images/1ydfa.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <p class="card-text">6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
+    <button className="btn-1">Read More</button>
+  </div>
+  </div>
+  <div class="card">
+  <img src="https://www.nifafinearts.com/images/2ydfa.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <p class="card-text">6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
+    <button className="btn-1">Read More</button>
+  </div>
+  </div>
+</div>
+    </div>
+  );
+};
+export default Header;
